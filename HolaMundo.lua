882b2038dda4bf2a74c6597051d0814cd2fd381d
Claurap <claurap@ulasalle.edu.pe>
2026-07-02 01:25:29 -0500

    Create HolaMundo.lua
    aea

---
+++ HolaMundo.lua
@@ -0,0 +1,14 @@
+--------------------------------------------------
+-- Hola Mundo en Luau
+-- Autor: Cristopher
+--------------------------------------------------
+
+-- Función principal
+local function main()
+
+	print("¡Hola Mundo!")
+
+end
+
+-- Ejecutar programa
+main()
